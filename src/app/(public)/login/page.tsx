78fcba1f7873bf48f03ae40890a811116611b261
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2025-04-28 23:53:50 -0300

    feat(login): add form with react-hook-form and validations with zod

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -16,22 +16,46 @@ import Image from "next/image";
 import logoCora from "@/assets/cora.jpg";
 import { useRouter } from "next/navigation";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 export default function Login() {
 	const router = useRouter();
 	const [showPassword, setShowPassword] = useState(false);
 
-	async function handleLogin() {
-		await getData(); // Chama a função e espera terminar
+	const loginSchema = z.object({
+		username: z.string().nonempty("Campo obrigatório"),
+		password: z.string().nonempty("Campo obrigatório"),
+	});
+
+	type LoginSchema = z.infer<typeof loginSchema>;
+
+	const { register, handleSubmit } = useForm({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			password: "",
+			username: "",
+		},
+	});
+
+	async function handleLogin({ password, username }: LoginSchema) {
+		console.log({ password, username });
+
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		console.log("Dados enviado");
+
+		document.cookie = "token=testeteste";
 		router.push("/");
 	}
 
-	async function getData() {
-		// Simulando uma requisição async (tipo login na API)
-		console.log("entrou na requisição");
-		await new Promise((resolve) => setTimeout(resolve, 1000)); // Espera 1 segundo
-		console.log("saiu da requisição");
-		document.cookie = "token=teste";
-	}
+	// async function getData() {
+	// 	// Simulando uma requisição async (tipo login na API)
+	// 	console.log("entrou na requisição");
+	// 	await new Promise((resolve) => setTimeout(resolve, 1000)); // Espera 1 segundo
+	// 	console.log("saiu da requisição");
+	// 	document.cookie = "token=teste";
+	// }
 
 	return (
 		<div className="min-h-screen flex justify-center items-center px-4 bg-[#6B5B95]">
@@ -47,42 +71,48 @@ export default function Login() {
 					</CardDescription>
 
 					<CardContent className="w-[100%] max-w-[400px] flex flex-col gap-5">
-						<div className="flex flex-col gap-2">
-							<Label htmlFor="usuario">Usuário</Label>
-							<Input id="usuario" placeholder="Digite seu usuário" />
-						</div>
-
-						<div className="flex flex-col gap-2">
-							<Label htmlFor="password">Senha</Label>
-							<div className="flex items-center border rounded-md">
+						<form onSubmit={handleSubmit(handleLogin)}>
+							<div className="flex flex-col gap-2">
+								<Label htmlFor="usuario">Usuário</Label>
 								<Input
-									id="password"
-									name="password"
-									type={showPassword ? "text" : "password"}
-									placeholder="Digite sua senha"
-									className="flex-1 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+									id="usuario"
+									placeholder="Digite seu usuário"
+									{...register("username")}
 								/>
-								<Button
-									type="button"
-									variant="ghost"
-									size="icon"
-									onClick={() => setShowPassword(!showPassword)}
-									className="h-auto w-auto p-2"
-								>
-									{showPassword ? (
-										<Eye className="w-5 h-5" />
-									) : (
-										<EyeClosed className="w-5 h-5" />
-									)}
+							</div>
+
+							<div className="flex flex-col gap-2">
+								<Label htmlFor="password">Senha</Label>
+								<div className="flex items-center border rounded-md">
+									<Input
+										id="password"
+										type={showPassword ? "text" : "password"}
+										placeholder="Digite sua senha"
+										className="flex-1 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+										{...register("password")}
+									/>
+									<Button
+										type="button"
+										variant="ghost"
+										size="icon"
+										onClick={() => setShowPassword(!showPassword)}
+										className="h-auto w-auto p-2"
+									>
+										{showPassword ? (
+											<Eye className="w-5 h-5" />
+										) : (
+											<EyeClosed className="w-5 h-5" />
+										)}
+									</Button>
+								</div>
+							</div>
+
+							<div className="flex gap-4 mt-4">
+								<Button className="flex-1" type="submit">
+									Entrar
 								</Button>
 							</div>
-						</div>
-
-						<div className="flex gap-4 mt-4">
-							<Button className="flex-1" type="submit" onClick={handleLogin}>
-								Entrar
-							</Button>
-						</div>
+						</form>
 					</CardContent>
 				</CardHeader>
 			</Card>
